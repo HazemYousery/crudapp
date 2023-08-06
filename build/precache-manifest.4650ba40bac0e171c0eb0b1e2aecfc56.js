8d27f5d7cec17b61ae7ec1f5abb0f2aba7eea464
HazemYousery <110838757+HazemYousery@users.noreply.github.com>
2023-08-06 13:57:24 +0300

    crud app files

---
+++ build/precache-manifest.4650ba40bac0e171c0eb0b1e2aecfc56.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b58966635c7e4320b9b2",
+    "url": "/static/css/main.b35f1d43.chunk.css"
+  },
+  {
+    "revision": "b58966635c7e4320b9b2",
+    "url": "/static/js/main.b5896663.chunk.js"
+  },
+  {
+    "revision": "df2e0fb9982dc7c0f4ef",
+    "url": "/static/js/1.df2e0fb9.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "bbd00a0eb193296437169605bfd0ea9b",
+    "url": "/index.html"
+  }
+];
